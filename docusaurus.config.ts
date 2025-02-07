913d313d1dcbdb2bcafe45bafbbb91117fe8182d
Sarita Himthani <sarita.himthani123@gmail.com>
2025-02-06 23:20:35 -0500

    cosmetic changes alongside styling

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -104,7 +104,7 @@ const config: Config = {
           items: [
             {
               label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
+              href: 'https://discord.gg/BREGCE94',
             }
           ],
         },
@@ -122,7 +122,7 @@ const config: Config = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} Pace Data Science Club. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} Pace Data Science Club.`,
     },
     prism: {
       theme: prismThemes.github,
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,7 +21,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Call to action button (Follow)
+            Read More
           </Link>
         </div>
       </div>
